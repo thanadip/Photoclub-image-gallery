a519bfcd1122f28fd8ab6bc420790d0cebb3f3d4
Thana Dip <thana.devproj@gmail.com>
2023-06-20 03:04:03 +0700

    adding CEO

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,20 @@ import "./App.css";
 function App() {
   const [count, setCount] = useState(0);
 
-  return <></>;
+  return (
+    <>
+      <div className="flex flex-col justify-center items-center h-screen">
+        <img
+          src="https://images.unsplash.com/photo-1514888286974-6c03e2ca1dba?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Y2F0fGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60"
+          alt="NULL"
+          className="w-max h-auto "
+        />
+        <h1>This is CEO</h1>
+
+        <h1> His name is raccoon</h1>
+      </div>
+    </>
+  );
 }
 
 export default App;
