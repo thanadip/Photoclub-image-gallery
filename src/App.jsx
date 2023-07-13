560cab7703e985f01e3d2b8986cc01d685bbb86d
Thana <Thana.devproj@gmail.com>
2023-07-13 14:06:29 +0700

    add MUI and register page

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import Navbar from "./components/Navbar";
 import About from "./components/about";
 import Contact from "./components/contact";
 import MusicBG from "./components/MusicBG";
+import Register from "./components/Register";
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
         <Routes>
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
+          <Route path="/register" element={<Register />} />
         </Routes>
       </BrowserRouter>
       <MusicBG />
---
+++ src/components/Register.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Register() {
+  return (
+    <div className="bg-neutral-700">
+      
+    </div>
+  )
+}
+
+
+
+export default Register
