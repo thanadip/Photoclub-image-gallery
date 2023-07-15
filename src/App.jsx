c2abb8ab6842d4123ce2d00215f42044caa416c3
Thana <Thana.devproj@gmail.com>
2023-07-15 22:27:14 +0700

    finished get started page and links

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,26 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 //components
 
 //pages
 import Start from "./pages/Start";
+import Contact from "./pages/Contact";
+import About from "./pages/About";
 
 
 function App() {
   return (
-    <>
-    <Start/>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Start />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/pages/Start.jsx
+++ src/pages/Start.jsx
@@ -1,13 +1,25 @@
 import React from 'react';
-import background from '../assets/startYod.jpg';
+import { Flex, Box, Text, Stack, Button, ButtonGroup } from '@chakra-ui/react';
 
 function Start() {
   return (
-    <>
-      <div>
-        <img src={background} alt="Start Yod" />
-      </div>
-    </>
+    <Flex justifyContent='center' alignItems='center' height='100vh'>
+      <Box width='60vw'>
+        <Stack textAlign='center'>
+          <Text fontSize='6xl'>Patipon Sombut</Text>
+          <Text fontSize='5xl'>Commission gallery</Text>
+          <ButtonGroup spacing='4' mt='8' ml='auto' mr='auto'>
+          <Button colorScheme='teal' w='175px' h='50px'>
+            Get Started
+          </Button>
+          <Text fontSize='15px' color='gray' mt='auto' mb='auto'>or</Text>
+          <Button as='a' href ='https://www.instagram.com/image_yoddoy.jpg/' target='_blank' color='#C13584' w='175px' h='50px' variant='outline'>
+          Instagram
+          </Button>
+        </ButtonGroup>
+        </Stack>
+      </Box>
+    </Flex>
   );
 }
 
