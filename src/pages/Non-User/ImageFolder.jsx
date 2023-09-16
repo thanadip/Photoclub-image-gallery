ae593ada9ba8cc0bdbb2795d423edfe468943465
Thana <Thana.devproj@gmail.com>
2023-09-17 02:43:32 +0700

    i am done with this backend bitch

--- src/pages/Non-User/ImageFolder.jsx
+++ src/pages/Non-User/ImageFolder.jsx
@@ -1,47 +1,58 @@
 import React, { useEffect, useState } from 'react';
 import Navbar from '../../components/Navbar';
 import axios from 'axios';
-import { useParams, Link } from 'react-router-dom';
-import { Flex, Text } from '@chakra-ui/react';
-
+import { useParams } from 'react-router-dom';
+import { Flex, List, ListItem, Text } from '@chakra-ui/react';
+import 'react-photo-view/dist/react-photo-view.css';
+import { PhotoProvider, PhotoView } from 'react-photo-view';
 
 function ImageFolder() {
   const [images, setImages] = useState([]);
   const { folderId } = useParams();
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
 
   useEffect(() => {
-    axios.get(`http://localhost:5001/get-images/${folderId}`)
-      .then(response => {
+    axios
+      .get(`http://localhost:5001/get-images/${folderId}`)
+      .then((response) => {
         setImages(response.data);
-        console.log(response.data)
+        console.log(response.data);
       })
-      .catch(error => {
+      .catch((error) => {
         console.error(error);
       });
   }, [folderId]);
 
-  const galleryImages = images.map((image ) => ({
-    original: `data:image/png;base64,${image.pic_name}`,
-    thumbnail: `data:image/png;base64,${image.pic_name}`
-  }));
-
-  const isEmpty =  galleryImages.length === 0;
+  const handleImageClick = (index) => {
+    setCurrentImageIndex(index);
+  };
 
   return (
     <>
       <Navbar />
-      <Flex direction="column" align="center">
-        <Text fontSize="xl" fontWeight="bold" mb="2">List of Images:</Text>
+      <Flex direction="column" align="center" maxH={'200px'} maxW={'200px'} mx={'auto'}>
+        <Text fontSize="xl" fontWeight="bold" mb="2">
+          List of Images:
+        </Text>
 
-        {isEmpty ? (
-          <Text> No image display</Text>
-        ) : (
-          <Text> Some image display</Text>
-        )
-        }
-
-        
-        
+        <PhotoProvider
+          currentIndex={currentImageIndex}
+          onClick={handleImageClick}
+          speed={() => 800}
+        >
+          <List spacing="2">
+            {images.map((image, index) => (
+              <ListItem key={index}>
+                <PhotoView src={`data:image/png;base64,${image.pic_name}`}>
+                  <img
+                    src={`data:image/png;base64,${image.pic_name}`}
+                    alt={`Image ${index}`}
+                  />
+                </PhotoView>
+              </ListItem>
+            ))}
+          </List>
+        </PhotoProvider>
       </Flex>
     </>
   );
