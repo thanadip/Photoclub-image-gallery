11e29285da05761f318be9b6dda524a20605f5b7
Thana <Thana.devproj@gmail.com>
2023-09-25 04:57:17 +0700

    just format on save

--- src/pages/Non-User/Contact.jsx
+++ src/pages/Non-User/Contact.jsx
@@ -1,61 +1,64 @@
-import React from 'react'
-import Navbar from '../../components/Navbar'
-import { Stack, Card, Flex, Text, Box } from '@chakra-ui/react'
+import React from "react";
+import Navbar from "../../components/Navbar";
+import { Stack, Card, Flex, Text, Box } from "@chakra-ui/react";
 
 function contact() {
   return (
     <>
-    <Navbar/>
+      <Navbar />
 
-    <Flex h={'100%'} direction={'column'} alignItems={'center'} mt={'100px'} >
+      <Flex h={"100%"} direction={"column"} alignItems={"center"} mt={"100px"}>
+        <Stack mx={"auto"} textAlign={"center"}>
+          <Text fontSize={"4xl"}> Contact us</Text>
+          <Text fontSize={"2xl"}>
+            {" "}
+            Have any question? We'd love to hear from you.
+          </Text>
+        </Stack>
 
-    <Stack mx={'auto'} textAlign={'center'}>
+        <Flex
+          mt={"70px"}
+          w={"75dvw"}
+          justifyContent={"space-around"}
+          textAlign={"center"}
+        >
+          <Card
+            bg={""}
+            h={"300px"}
+            w={"250px"}
+            borderTop={"8px"}
+            borderTopColor={"#1877f2"}
+            _hover={{ transform: "scale(1.05)", transition: "transform 0.3s" }}
+          >
+            Facebook
+          </Card>
 
-      <Text fontSize={'4xl'}> Contact us</Text>
-      <Text fontSize={'2xl'}> Have any question? We'd love to hear from you.</Text>
-    
-    </Stack>
+          <Card
+            bg={""}
+            h={"350px"}
+            w={"250px"}
+            mt={"20px"}
+            borderTop={"8px"}
+            borderTopColor={"gray"}
+            _hover={{ transform: "scale(1.05)", transition: "transform 0.3s" }}
+          >
+            Contact information
+          </Card>
 
-    <Flex mt={'70px'} w={'75dvw'} justifyContent={'space-around'} textAlign={'center'}>
-
-      <Card 
-      bg={''} 
-      h={'300px'} 
-      w={'250px'}  
-      borderTop={'8px'} 
-      borderTopColor={'#1877f2'} 
-      _hover={{ transform: 'scale(1.05)', transition: 'transform 0.3s' }} >
-        Facebook
-        </Card>
-
-      <Card 
-      bg={''} 
-      h={'350px'} 
-      w={'250px'} 
-      mt={'20px'} 
-      borderTop={'8px'} 
-      borderTopColor={'gray'}
-      _hover={{ transform: 'scale(1.05)', transition: 'transform 0.3s' }}>
-        Contact information
-        </Card>
-
-      <Card 
-      bg={''} 
-      h={'300px'} 
-      w={'250px'} 
-      borderTop={'8px'} 
-      borderTopColor={'#00b900'}
-      _hover={{ transform: 'scale(1.05)', transition: 'transform 0.3s' }}>
-        Line
-        </Card>
-    
-    </Flex>
-
-    </Flex>
-
-    
+          <Card
+            bg={""}
+            h={"300px"}
+            w={"250px"}
+            borderTop={"8px"}
+            borderTopColor={"#00b900"}
+            _hover={{ transform: "scale(1.05)", transition: "transform 0.3s" }}
+          >
+            Line
+          </Card>
+        </Flex>
+      </Flex>
     </>
-  )
+  );
 }
 
-export default contact
+export default contact;
--- src/pages/Non-User/Index.jsx
+++ src/pages/Non-User/Index.jsx
@@ -1,62 +1,77 @@
-import React, { useState, useEffect } from 'react';
-import Navbar from '../../components/Navbar';
-import axios from 'axios';
-import { Link , useNavigate} from 'react-router-dom';
-import { Flex, List, ListItem, Text , Card, CardBody, CardHeader, Heading, Stack, StackDivider, Box} from '@chakra-ui/react';
+import React, { useState, useEffect } from "react";
+import Navbar from "../../components/Navbar";
+import axios from "axios";
+import { Link, useNavigate } from "react-router-dom";
+import {
+  Flex,
+  List,
+  ListItem,
+  Text,
+  Card,
+  CardBody,
+  CardHeader,
+  Heading,
+  Stack,
+  StackDivider,
+  Box,
+} from "@chakra-ui/react";
 
 function UserHome() {
-
-  const [years , setYears] = useState([]);
+  const [years, setYears] = useState([]);
   const navigate = useNavigate();
 
-  const handleYearClick = async (year_id) =>{
+  const handleYearClick = async (year_id) => {
     try {
-    
-      const response = await fetch(`http://localhost:5001/get-folders/${year_id}`);
+      const response = await fetch(
+        `http://localhost:5001/get-folders/${year_id}`
+      );
       const data = await response.json();
-      console.log('OK',data)
+      console.log("OK", data);
       navigate(`/images/${year_id}`);
     } catch (error) {
-      console.log('An error occurred: ', error)
+      console.log("An error occurred: ", error);
     }
-  }
+  };
 
   useEffect(() => {
-    axios.get('http://localhost:5001/get-year')
-      .then(response => {
+    axios
+      .get("http://localhost:5001/get-year")
+      .then((response) => {
         setYears(response.data);
       })
-      .catch(error => {
-        console.error('Error fetching years:', error);
+      .catch((error) => {
+        console.error("Error fetching years:", error);
       });
   }, []);
 
   return (
     <>
       <Navbar />
-      <Flex direction="column" align="left"  mt='8dvh' maxW={'40vw'}>
+      <Flex direction="column" align="left" mt="8dvh" maxW={"40vw"}>
+        <Card>
+          <CardHeader>
+            <Heading size="md">เลือกปีการศึกษา:</Heading>
+          </CardHeader>
 
-<Card>
-  <CardHeader>
-    <Heading size='md'>เลือกปีการศึกษา:</Heading>
-  </CardHeader>
-
-  <CardBody >
-    <Stack divider={<StackDivider />} spacing='4' >
-      {years.map(year => (
-        <Box as={Link} onClick={() => handleYearClick(year.year_id)} _hover={{bgColor:'red'}} >
-        <Heading size='L' textTransform='uppercase'>
-          ปีการศึกษา {year.year_name}
-        </Heading>
-        <Text pt='2' fontSize='sm'>
-          รวมกิจกรรมในปีการศึกษา {year.year_name}
-        </Text>
-      </Box>
-      ))}
-      
-    </Stack>
-  </CardBody>
-</Card>
+          <CardBody>
+            <Stack divider={<StackDivider />} spacing="4">
+              {years.map((year) => (
+                <Box
+                  onClick={() => handleYearClick(year.year_id)}
+                  _hover={{ bgColor: "red" }}
+                  cursor={"pointer"}
+                >
+                  <Heading size="L" textTransform="uppercase">
+                    ปีการศึกษา {year.year_name}
+                  </Heading>
+                  <Text pt="2" fontSize="sm">
+                    รวมกิจกรรมในปีการศึกษา {year.year_name}
+                  </Text>
+                </Box>
+              ))}
+            </Stack>
+          </CardBody>
+        </Card>
       </Flex>
     </>
   );
